ac939ba5a10123e5e9c4fcf615e76c960ff9e965
johnoro <officialjohnorourke@gmail.com>
2019-02-13 18:39:52 -0500

    Finish Country class

--- src/main/java/com/lambdaschool/countries/Country.java
+++ src/main/java/com/lambdaschool/countries/Country.java
@@ -1,5 +1,47 @@
 package com.lambdaschool.countries;
 
 public class Country {
+  String name;
+  int population;
+  int landMassSize;
+  int ageMedian;
 
+  public Country(String name, int population, int size, int age) {
+    this.name = name;
+    this.population = population;
+    landMassSize = size;
+    ageMedian = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPopulation() {
+    return population;
+  }
+
+  public void setPopulation(int population) {
+    this.population = population;
+  }
+
+  public int getLandMassSize() {
+    return landMassSize;
+  }
+
+  public void setLandMassSize(int size) {
+    landMassSize = size;
+  }
+
+  public int getAgeMedian() {
+    return ageMedian;
+  }
+
+  public void setAgeMedian(int age) {
+    ageMedian = age;
+  }
 }
