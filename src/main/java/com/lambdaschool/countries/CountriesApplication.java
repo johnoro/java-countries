836f2f9d1ecddbbd62622a6c194a596d59b752eb
johnoro <officialjohnorourke@gmail.com>
2019-02-13 18:47:36 -0500

    Add CountryList class

--- src/main/java/com/lambdaschool/countries/CountriesApplication.java
+++ src/main/java/com/lambdaschool/countries/CountriesApplication.java
@@ -2,10 +2,16 @@ package com.lambdaschool.countries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class CountriesApplication {
+  static ArrayList<Country> countryList;
   public static void main(String[] args) {
+    countryList = new CountryList().countryList;
+
+    // To-do: Add CountryController
+
     SpringApplication.run(CountriesApplication.class, args);
   }
 }
